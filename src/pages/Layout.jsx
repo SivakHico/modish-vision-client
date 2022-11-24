c19db00e19a0d5890faca514cd5b99f05cf2d54e
Sivak Hico <strukggg@gmail.com>
2022-11-24 11:27:22 +0100

    update login as admin

--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -1,12 +1,27 @@
 import { Outlet, Link } from "react-router-dom";
-import { useState } from "react";
+import { useState } from "react"
+import axios from "axios";
 
 const Layout = () => {
+    const [user, setUser] = useState(null);
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
     const [getShow, setShow] = useState(false)
 
     function handelDisplay() {
         setShow(!getShow)
     }
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            const res = await axios.post("http://localhost:3000/api/v1/users/login", { email, password });
+            setUser(res.data.user);
+            handelDisplay()
+        } catch (err) {
+            console.log(err);
+        }
+    };
 
     return (
         <>
@@ -24,21 +39,26 @@ const Layout = () => {
                         <div className="w3-center"><br />
                             <span onClick={handelDisplay} className="w3-button w3-xlarge w3-hover-red w3-display-topright" title="Close Modal">&times;</span>
                         </div>
-                        <form className="w3-container" action="">
+                        <form className="w3-container" onSubmit={handleSubmit}>
                             <div className="w3-section">
-                                <input className="w3-input w3-border w3-margin-bottom" type="text" placeholder="Enter Username" name="usrname" required />
-                                <input className="w3-input w3-border" type="password" placeholder="Enter Password" name="psw" required />
+                                <input className="w3-input w3-border w3-margin-bottom" onChange={(e) => setEmail(e.target.value)} type="text" placeholder="Email" name="email" required />
+                                <input className="w3-input w3-border" onChange={(e) => setPassword(e.target.value)} type="password" placeholder="Password" name="password" required />
                                 <button className="w3-button w3-block w3-green w3-section w3-padding" type="submit">Login</button>
                                 <input className="w3-check w3-margin-top" type="checkbox" /> Remember me
                             </div>
                         </form>
                         <div className="w3-container w3-border-top w3-padding-16 w3-light-grey">
                             <button onClick={handelDisplay} type="button" className="w3-button w3-red">Cancel</button>
-                            <span className="w3-right w3-padding w3-hide-small">Forgot <a href="#">password?</a></span>
+                            <span className="w3-right w3-padding w3-hide-small">Forgot <a href="#">Password?</a></span>
                         </div>
                     </div>
                 </div>
             }
+            { user ?
+                <span>
+                    Welcome to the <b>{user.isAdmin ? "admin" : "user"}</b>
+                    <b>{user.email}</b>
+                </span> : 'Fcuk'}
             <Outlet />
         </>
     )
